357e31e4eb6061c615c6e8874f29ec6f802a65c6
Kyle Reicks <reicks.kyle@gmail.com>
2013-02-27 21:29:41 -0600

    Initialize plugin and create initial class.

---
+++ timeline-wp.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Timeline.WP
+Plugin URI: http://github.com/kylereicks/timeline.js.wp
+Description: A wordpress plugin to create timeline.js timelines to wordpress.
+Author: Kyle Reicks
+Version: 0.1
+Author URI: http://kylereicks.me
+*/
+
+if(!class_exists(Timeline_WP)){
+  class Timeline_WP{
+
+  }
+  $timeline_wp = new Timeline_WP();
+}
